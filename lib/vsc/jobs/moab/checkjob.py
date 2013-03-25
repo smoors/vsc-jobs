00c272ed1311b773bd9372ce2d9b3f1c11e50b6d
Andy Georges <itkovian@gmail.com>
2013-03-25 17:09:23 +0100

    Added display option

--- lib/vsc/jobs/moab/checkjob.py
+++ lib/vsc/jobs/moab/checkjob.py
@@ -18,13 +18,13 @@ All things checkjob.
 
 @author Andy Georges
 """
+import yaml
 
 from lxml import etree
 
 from vsc.jobs.moab.internal import MoabCommand
 from vsc.utils.fancylogger import getLogger
 from vsc.utils.missing import RUDict
-from vsc.utils.run import RunAsyncLoop
 
 
 logger = getLogger('vsc.jobs.moab.checkjob')
@@ -42,12 +42,34 @@ class CheckjobInfo(RUDict):
     def __init__(self, *args, **kwargs):
         super(CheckjobInfo, self).__init__(*args, **kwargs)
 
+        self.logger = getLogger(self.__class__.__name__)
+
     def add(self, user, host):
 
         if not user in self:
             self[user] = RUDict()
         if not host in self[user]:
             self[user][host] = []
+
+    def display(self, jobid=None):
+        """Display the contents of the data for the given job id.
+
+        If the job id is None, all results are given.
+        """
+        if not jobid:
+            return yaml.dump(self, default_flow_style=False)
+
+        location = [(user, host) for user in self for host in self[user] if jobid in self[user][host]]
+
+        if not location:
+            self.logger.debug("No matching location in the job tree for job id %s" % (jobid))
+            return ""
+
+        if len(location) > 1:
+            self.logger.error("Nultiple matching locations in the job tree for job id %s: %s" % (jobid, location))
+            return None
+
+        return yaml.dump(self[location[0]][location[1]][jobid], default_flow_style=False)
 
 
 class Checkjob(MoabCommand):
@@ -65,7 +87,6 @@ class Checkjob(MoabCommand):
 
     def parser(self, host, txt):
         """Parse the checkjob XML and produce a corresponding CheckjobInfo instance."""
-
         xml = etree.fromstring(txt)
 
         checkjob_info = CheckjobInfo()
