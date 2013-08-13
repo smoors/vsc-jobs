993e1a4ff27738936b8a60d040a3003b186d62d9
stdweird <stijn.deweirdt@ugent.be>
2013-08-13 20:30:22 +0200

    version bump

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ shared_setup.SHARED_TARGET.update({
 
 PACKAGE = {
     'name': 'vsc-jobs',
-    'version': '0.5.6',
+    'version': '0.5.7',
     'author': [sdw, ag],
     'maintainer': [sdw, ag],
     'packages': ['vsc', 'vsc.jobs', 'vsc.jobs.moab', 'vsc.jobs.pbs'],
