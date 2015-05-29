d2d6728da7e86b9ea3330fabd584f4f5d81a0fba
Andy Georges <itkovian@gmail.com>
2015-05-29 14:58:41 +0200

    Tests for qstat_wrapper skeleton HPC-5237

---
+++ tests/qstat_wrapper.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+##
+# Copyright 2015-2015 Ghent University
+#
+# This file is part of vsc-jobs,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# All rights reserved.
+#
+##
+"""
+@author: Andy Georges (Ghent University)
+"""
+
+from unittest import TestCase
+
+
+class TestQstatWrapper(TestCase):
+    pass
