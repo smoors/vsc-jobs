546fa233bee02bad090bedefeb3ef5147a9bbb3e
Andy Georges <itkovian@gmail.com>
2015-05-29 15:57:49 +0200

    Moved out functions to a library module HPC-5237

--- bin/qstat_wrapper.py
+++ bin/qstat_wrapper.py
@@ -20,119 +20,14 @@ lines if needed.
 
 @author: Andy Georges (Ghent University)
 """
-import itertools
+import sys
 
-from collections import defaultdict
 from PBSQuery import PBSQuery
+from vsc.jobs.pbs.qstat import transform_info
 from vsc.utils import fancylogger
 from vsc.utils.generaloption import simple_option
 
-PBS_EXEC_HOST = "exec_host"
-PBS_CPUTIME = 'resources_used.cput'
-PBS_WALLTIME = 'resources_used.walltime'
-
-
-def ranges(numbers):
-    """
-    Taken from http://stackoverflow.com/questions/4628333/converting-a-list-of-integers-into-range-in-python
-    """
-    for a, b in itertools.groupby(enumerate(numbers), lambda (x, y): y - x):
-        b = list(b)
-        yield b[0][1], b[-1][1]
-
-
-def convert_to_range(numbers):
-    """
-    Given a set of numbers, convert this to a string that condenses the
-    set into ranges, where possible.
-
-    For example: 1,2,3,6,7,8,9,12,23,24 will become 1-3,6-9,12,23-24
-    """
-    range = list()
-    for (a, b) in list(ranges(sorted(numbers))):
-        if a == b:
-            range.append("%d" % a)
-        else:
-            range.append("%d-%d" % (a, b))
-
-    return ",".join(range)
-
-
-def normalise_exec_host(nodes):
-    """
-    Convert a list of nodes nodeXYZ/C into nodeXYZ/C-D format.
-    """
-    nodecores = defaultdict(set)
-
-    # workaround for the format provided by PBSQuery
-    if isinstance(nodes, list) and len(nodes) == 1:
-        nodes_ = nodes[0].split("+")
-        if len(nodes_) > 1:
-            nodes = nodes_
-
-    for node in nodes:
-        (name, core) = node.split("/")
-        nodecores[name].add(int(core))
-
-    s = list()
-    for node in sorted(nodecores.keys()):
-        range = convert_to_range(nodecores[node])
-        s.append("%s/%s" % (node, range))
-
-    return ",".join(s)
-
-
-def normalise_time(time):
-    """
-    Convert a PBS time to a time in seconds
-    """
-
-    # workaround for the format provided by PBSQuery
-    if isinstance(time, list):
-        time = time[0]
-
-    parts = [int(t) for t in time.split(":")]
-    parts.reverse()
-
-    seconds = 0
-    multiplier = 1
-    for t in parts[:3]:
-        seconds += t * multiplier
-        multiplier *= 60
-
-    if len(parts) > 3:
-        seconds += 24 * 60 * 60 * parts[3]
-
-    return seconds
-
-
-def print_info(job, info):
-    """
-    Print the information in the job structure for the requested info items and reformat the data
-
-    The info string is a comma-separated list with items of the form input_key:output_key. The input_key
-    is used to index the job dictionary structure PBSQuery provides. The output_key will be used as a prefix
-    on the line that is printed with the transformed information.
-    """
-    transformers = {
-        PBS_EXEC_HOST: normalise_exec_host,
-        PBS_CPUTIME: normalise_time,
-        PBS_WALLTIME: normalise_time,
-    }
-
-    keys = [k.strip().split(":") for k in info.split(",")]
-
-    for (input_key, output_key) in keys:
-        try:
-            (component, item) = input_key.split(".")
-            value = job[component][item]
-        except ValueError:
-            value = job[input_key]
-
-        try:
-            print "%s: %s" % (output_key, transformers[input_key](value))
-        except KeyError:
-            print "%s: %s" % (output_key, value)
+logger = fancylogger.getLogger(sys.args[0])
 
 
 def main():
@@ -142,13 +37,19 @@ def main():
 
     options = {
         "jobid": ("The PBS_JOBID of the job for which we want information", None, "store", None),
-        "information": ("Comma-separated list of the job info to print", None, "store", None),
+        "information": ("Comma-separated list of the job info to print. "
+                        "Entries of the format input_key:output_key", None, "store", None),
     }
     opts = simple_option(options)
+
+    if not opts.options.jobid:
+        logger.error("jobid is a required option. Bailing.")
+        sys.exit(1)
+
     pquery = PBSQuery()
     current_job = pquery.getjob(opts.options.jobid)
 
-    print_info(current_job, opts.options.information)
+    transform_info(current_job, opts.options.information)
 
 
 main()
