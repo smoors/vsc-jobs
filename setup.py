070f08ffe1d5b1f104866bb21a864b4d01b6110b
stdweird <stijn.deweirdt@ugent.be>
2017-11-22 13:28:54 +0100

    update requirements

--- setup.py
+++ setup.py
@@ -37,12 +37,9 @@ PACKAGE = {
         # use local install from https://oss.trac.surfsara.nl/pbs_python/ticket/41#attachments
         # or rpms
         # 'pbs_python >= 4.6',
-        'vsc-administration >= 0.20.1',
+        'vsc-administration >= 1.1.0',
         'vsc-accountpage-clients >= 0.1.2',
-        'vsc-base >= 2.4.2',
-        'vsc-config >= 1.26',
         'vsc-ldap >= 1.3.4',
-        #'vsc-ldap-extension >= 1.10',
         'vsc-utils >= 1.4.6',
         'lxml',
     ],
