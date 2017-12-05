8f8366b3322046a939937727a4d2a4bf3c829d77
Andy Georges <itkovian@gmail.com>
2017-12-05 15:39:21 +0100

    Version bump to 0.20.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ vsc-jobs base distribution setup.py
 import vsc.install.shared_setup as shared_setup
 from vsc.install.shared_setup import ag, sdw, jt
 
-VERSION = '0.20.0'
+VERSION = '0.20.1'
 
 PACKAGE = {
     'version': VERSION,
